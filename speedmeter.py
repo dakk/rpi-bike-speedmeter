0af6079132067bedc83d7fd442e23f29c2e36b64
Davide Gessa <gessadavide@gmail.com>
2015-07-08 15:58:37 +0200

    update

--- speedmeter.py
+++ speedmeter.py
@@ -5,9 +5,17 @@ import time
 wiringpi.wiringPiSetupGpio()
 
 wiringpi.pinMode(17, 1)
-
+cs = 0
+ST = 0.01
+ones = 0
 
 while True:
-	time.sleep (0.1)
-	print wiringpi.digitalRead(17)
-
+	while cs < 1/ST:
+		time.sleep (ST)
+		v = wiringpi.digitalRead(17)
+		cs += 1
+		if int (v) == 1:
+			ones += 1
+	print 'Contact in one second:',ones
+	ones = 0
+	cs = 0
