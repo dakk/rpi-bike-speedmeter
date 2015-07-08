2be428fd0c15fc847e8b96b2dc4ca0fb7abc4a71
Davide Gessa <gessadavide@gmail.com>
2015-07-08 16:06:21 +0200

    update

--- speedmeter.py
+++ speedmeter.py
@@ -2,20 +2,31 @@
 import wiringpi
 import time
 
+# Sleep time
+ST = 0.01
+
+# Wheel size
+WS = 26.0
+
+
+
 wiringpi.wiringPiSetupGpio()
 
 wiringpi.pinMode(17, 1)
 cs = 0
-ST = 0.01
-ones = 0
+rounds = 0
 
 while True:
 	while cs < 1/ST:
 		time.sleep (ST)
 		v = wiringpi.digitalRead(17)
 		cs += 1
-		if int (v) == 1:
-			ones += 1
-	print 'Contact in one second:',ones
-	ones = 0
+		rounds += int (v)
+
+	rpm = (rpm + (rounds * 60)) / 2
+
+	print 'Round per second:',rounds
+	print 'Round per minute:',rpm
+
+	rounds = 0
 	cs = 0
