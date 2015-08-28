5f9e0492007ed2ae3972f3db6f6660d76220e8b5
Davide Gessa <gessadavide@gmail.com>
2015-08-28 16:12:34 +0100

    update formula

--- speedmeter.py
+++ speedmeter.py
@@ -12,7 +12,7 @@ PORT = 6061
 ST = 0.01
 
 # Wheel radius (mm)
-WR = 260.0
+WR = 180.0
 
 # Circonference 2 * pi * r (m) and (km)
 CF = (2 * 3.14 * WR) / 1000.0
@@ -59,9 +59,9 @@ while True:
 			rounds += 1
 		pstate = int (v)
 
-	rpm = (rpm + (rounds * 60)) / 2
-	kmm = rpm * CFK
-	kmh = rpm * CFK * 60
+	rpm = (rpm + (rounds * 60.0)) / 2.0
+	kmm = rpm * CF
+	kmh = rpm * CF * 60
 	print 'RPS:',rounds,'RPM:',rpm,'km/m:',kmm,'km/h:',kmh
 
 	rounds = 0
